3ace6784d018f2649f3ad0241554846dd63d0862
Hugo Peixoto <hugo.peixoto@gmail.com>
2023-10-12 16:25:32 +0100

    Adds basic counter functionality

---
+++ app/controllers/counters_controller.rb
@@ -0,0 +1,19 @@
+class CountersController < ApplicationController
+  def show
+    @counter = Counter.first
+
+    render json: @counter.as_json(only: [:title, :active, :time_left, :time_started])
+  end
+
+  def edit
+    @counter = Counter.first_or_create!
+  end
+
+  def update
+    @counter = Counter.first_or_create!
+
+    @counter.update(params.require(:counter).permit(:title, :active, :time_left, :time_started))
+
+    redirect_to :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
   root "homepage#index"
   get 'info', to: 'information#index'
   get 'schedule', to: 'schedule#index'
   get 'timing', to: 'timing#index'
+
+  resource 'counter', only: %w[edit update show]
 end
